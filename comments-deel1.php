a72f04002deca3c30b3ba55820c4f7e91b9e1b89
stoicsteve <LocalAdmin@MECH-B107-11.syntraab.mechelen>
2015-08-10 13:12:09 +0200

    PHP-block en comment-block

---
+++ comments-deel1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/global.css">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/directory.css">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/facade.css">
+</head>
+
+
+<?php
+
+	$voornaam = 'Steven'; 
+	$achternaam = 'Leysen'; 
+	
+// Dit is commentaar
+	
+	/*
+	*
+	* Steven Leysen
+	*
+	*/
+?>
+<body class="web-backend-inleiding">
+
+	<section class="body">
+
+		<h1>Voorbeeld van PHP-commentaar</h1>
+			
+	    <p>Voornaam: <?= $voornaam ?></p>
+		<p>Achternaam: <?= $achternaam ?></p>
+
+	</section>
+
+</body>
+</html>
